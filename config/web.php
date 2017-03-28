8ca74ec6d658a4a641104bf98b66d461b22ac92d
twin1080 <twin1080@gmail.com>
2017-03-28 19:43:42 +0500

    And I forgot banned GII

--- config/web.php
+++ config/web.php
@@ -59,13 +59,13 @@ if (YII_ENV_DEV) {
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = [
-        'class' => 'yii\gii\Module',
+    //$config['bootstrap'][] = 'gii';
+    //$config['modules']['gii'] = [
+        //'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
 
-        'allowedIPs' => ['127.0.0.1', '::1'],
-    ];
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    //];
 }
 
 return $config;
